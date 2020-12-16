a67adc0a85dd781e58a8bd919c030623c7a08aa2
Kartik Buddha <kartik14@gmail.com>
2020-12-16 16:08:07 +0100

    Use loop to display all anomaly classes

--- app/app.py
+++ app/app.py
@@ -1,15 +1,10 @@
 import os
-import time
-import hashlib
 
-from flask import Flask, render_template, redirect, url_for, request, abort
+from flask import Flask, render_template, redirect, url_for, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from werkzeug.utils import secure_filename
 from wtforms import SubmitField
-
-from evaluate.evaluate_model import ModelEvaluator
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,6 +15,44 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
 
 # evaluator = ModelEvaluator('model.h5', (1, 40, 32, 3), 'rgb')
+
+CLASSES = [('No-Anomaly',
+            ' Nominal solar module',
+            'static/no-anomaly.png'),
+           ('Cell',
+            'Hot spot occurring with square geometry in single cell',
+            'static/cell.png'),
+           ('Cell-Multi',
+            'Hot spots occurring with square geometry in multiple cells',
+            'static/cell-multi.png'),
+           ('Cracking',
+            'Module anomaly caused by cracking on module surface',
+            'static/cracking.png'),
+           ('Hot-Spot',
+            'Hot spot on a thin film module',
+            'static/hot-spot.png'),
+           ('Hot-Spot-Multi',
+            'Multiple hot spots on a thin film module',
+            'static/hot-spot-multi.png'),
+           ('Shadowing',
+            'Sunlight obstructed by vegetation, man-made structures, or adjacent rows',
+            'static/shadowing.png'),
+           ('Diode',
+            'Activated bypass diode,'
+            ' typically 1/3 of module',
+            'static/diode.png'),
+           ('Diode-Multi',
+            'Multiple activated bypass diodes, typically affecting 2/3 of module',
+            'static/diode-multi.png'),
+           ('Vegetation',
+            'Panels blocked by vegetation',
+            'static/vegetation.png'),
+           ('Soiling',
+            'Dirt, dust, or other debris on surface of module',
+            'static/soiling.png'),
+           ('Offline-Module',
+            'Entire module is heated',
+            'static/offline-module.png')]
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
@@ -33,7 +66,7 @@ class UploadForm(FlaskForm):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', classes=CLASSES)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
