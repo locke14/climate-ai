1dd4a74f3777d6fcb314241f0c4482266c693a3f
Kartik Buddha <kartik14@gmail.com>
2020-12-16 14:44:40 +0100

    Add drag and drop form

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@ import os
 import time
 import hashlib
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, abort
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
 from wtforms import SubmitField
 
 from evaluate.evaluate_model import ModelEvaluator
@@ -15,6 +16,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I have a dream'
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'uploads')
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
+
 # evaluator = ModelEvaluator('model.h5', (1, 40, 32, 3), 'rgb')
 
 photos = UploadSet('photos', IMAGES)
@@ -35,37 +39,26 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     form = UploadForm()
-    if form.validate_on_submit():
-        for filename in request.files.getlist('photo'):
-            name = hashlib.md5(str('admin' + str(time.time())).encode('utf-8')).hexdigest()[:15]
-            photos.save(filename, name=name + '.')
-            prediction = 'Anomaly'
-            # prediction = evaluator.predict(f'uploads/{name}.jpg')
-            # print(prediction)
-        success = True
-    else:
-        prediction = 'Unknown'
-        success = False
-    return render_template('upload.html', form=form, success=success, prediction=prediction)
+    if request.method == 'POST':
+        request.files['file'].save(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], request.files['file'].filename))
+        return redirect(url_for('upload_file'))
 
-
-@app.route('/manage')
-def manage_file():
     files_list = os.listdir(app.config['UPLOADED_PHOTOS_DEST'])
-    return render_template('manage.html', files_list=files_list)
+    print(files_list)
+    file_urls = [photos.url(filename) for filename in files_list]
+    # evaluator.predict(f'uploads/{name}.jpg')
+    predictions = ['Vegetation' for filename in files_list]
 
-
-@app.route('/open/<filename>')
-def open_file(filename):
-    file_url = photos.url(filename)
-    return render_template('browser.html', file_url=file_url)
+    return render_template('upload.html',
+                           form=form,
+                           files=zip(files_list, file_urls, predictions))
 
 
 @app.route('/delete/<filename>')
 def delete_file(filename):
     file_path = photos.path(filename)
     os.remove(file_path)
-    return redirect(url_for('manage_file'))
+    return redirect(url_for('upload_file'))
 
 
 if __name__ == '__main__':
